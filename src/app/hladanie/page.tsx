06fda4f3299ea0afdf7a497a8d5792732fe73ab1
DenisVasek <vasekdenis@outlook.com>
2024-09-25 09:19:03 +0200

    TretiCommit

---
+++ src/app/hladanie/page.tsx
@@ -0,0 +1,18 @@
+
+// src/app/hladanie/page.tsx
+
+
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+
+export const metadata = { title: `Detail Profilu | KapuKakaKiki` };
+
+export default function ProfilDetail() {
+
+  return (
+    <Container>
+      <Typography> Detail Profilu </Typography>
+    </Container>
+  );
+
+}
